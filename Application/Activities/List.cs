fc0ba7031d834d1580b5ea952e58e98809af1f62
Jayce <jeancedryc.mestas@septeo.com>
2024-04-25 12:11:07 +0200

    End of section 14

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,4 +1,6 @@
 using Application.Core;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -8,16 +10,22 @@ namespace Application.Activities
 {
     public class List
     {
-        public class Query : IRequest<Result<List<Activity>>> {}
+        public class Query : IRequest<Result<List<ActivityDTO>>> {}
 
-        public class Handler : IRequestHandler<Query, Result<List<Activity>>> {
+        public class Handler : IRequestHandler<Query, Result<List<ActivityDTO>>> {
             private readonly DataContext _context;
-            public Handler(DataContext context){
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper){
                 _context = context;
+                _mapper = mapper;
             }
-            public async Task<Result<List<Activity>>> Handle(Query request, CancellationToken token) 
+            public async Task<Result<List<ActivityDTO>>> Handle(Query request, CancellationToken token) 
             {
-                return Result<List<Activity>>.Success(await _context.Activities.ToListAsync());
+                var activities = await _context.Activities
+                .ProjectTo<ActivityDTO>(_mapper.ConfigurationProvider)
+                .ToListAsync(token);
+
+                return Result<List<ActivityDTO>>.Success(activities);
             }
         }        
     }
