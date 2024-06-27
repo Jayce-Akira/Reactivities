98aa07e17570d4da92c1a31ea1bcc73b46481f00
Jayce <jeancedryc.mestas@septeo.com>
2024-06-27 12:04:06 +0200

    End of section 16

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -6,10 +6,13 @@ namespace Persistence
 {
     public class DataContext : IdentityDbContext<AppUser>
     {
-        public DataContext(DbContextOptions options) : base(options) {
+        public DataContext(DbContextOptions options) : base(options) 
+        {
         }
+        
         public DbSet<Activity> Activities { get; set; }
         public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+        public DbSet<Photo> Photos { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
